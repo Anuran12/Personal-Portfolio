b0626dfe73de4924357951625aa4de6742e9ad0c
Anuran12 <anuranarts@gmail.com>
2023-05-07 20:40:09 +0530

    update

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -44,10 +44,6 @@ const Footer = () => {
           <img src={images.email} alt="email" />
           <a href="mailto:anuranarts.com" className="p-text">anuranarts@gmail.com</a>
         </div>
-        <div className="app__footer-card">
-          <img src={images.mobile} alt="phone" />
-          <a href="tel:+919804793204" className="p-text">+91 9804793204</a>
-        </div>
       </div>
       {!isFormSubmitted ? (
         <div className="app__footer-form app__flex">
